dc06e08e922a32fb4fa9648db4dc513ef7349ca2
Sabrina Marques <sabrinamarques0240@gmail.com>
2024-02-26 09:33:30 -0300

    Mudar de cor

---
+++ scrip.js
@@ -0,0 +1,9 @@
+const titulo = document.getElementById("titulo")
+const botão = document.getElementsByTagName("mudarCor")
+
+function mudarCor(){
+    if(mudarCor){
+        titulo.style.backgroundColor = "green"
+
+}
+}
